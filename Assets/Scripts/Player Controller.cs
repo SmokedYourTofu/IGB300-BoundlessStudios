94bc545aca481588246dce5c3bddcd1d3e1a8917
SmokedYourTofu <93462741+SmokedYourTofu@users.noreply.github.com>
2024-04-18 13:39:13 +1000

    QOL + player rotation

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float rotationSpeed;
     public VariableJoystick variableJoystick;
     public Rigidbody rb;
 
     public void FixedUpdate() {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
         rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        if(direction != Vector3.zero) {
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
