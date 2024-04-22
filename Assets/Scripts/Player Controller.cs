fa15f100747b5f768e46c08920014a6172ec5abe
SmokedYourTofu <93462741+SmokedYourTofu@users.noreply.github.com>
2024-04-22 13:41:29 +1000

    added dash mechanic
    needs to be improved

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,17 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    [Header("Player Components")]
     public VariableJoystick variableJoystick;
     public Rigidbody rb;
+    public Transform orientation;
 
-    public void FixedUpdate() {
+    [Header("Movement")]
+    public float baseSpeed;
+    public float currentSpeed;
+    public float rotationSpeed;
+
+    [Header("Dashing")]
+    public float dashForce;
+    public float dashCooldown;
+    public float dashCooldownTimer;
+    public float speedPenalty;
+    public float speedPenaltyTimer;
+    public float speedPenaltyDuration;
+
+    private void Update() {
+        if(dashCooldownTimer > 0) {
+            dashCooldownTimer -= Time.deltaTime;
+        }
+
+        if(speedPenaltyTimer > 0) {
+            speedPenaltyTimer -= Time.deltaTime;
+        } else {
+            currentSpeed = baseSpeed;
+        }
+    }
+
+    private void FixedUpdate() {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rb.AddForce(direction * currentSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
         if(direction != Vector3.zero) {
             Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
@@ -21,5 +47,18 @@ public class PlayerController : MonoBehaviour
 
     public void Interact() {
         Debug.Log("Interacted");
+    }
+
+    public void Dash() {
+        // checks if dashing is on cooldown
+        if (dashCooldownTimer > 0) return;
+        else dashCooldownTimer = dashCooldown;
+
+        // Apply Dash force to player
+        Vector3 dash = orientation.forward * dashForce;
+        rb.AddForce(dash, ForceMode.VelocityChange);
+
+        speedPenaltyTimer = speedPenaltyDuration;
+        currentSpeed = speedPenalty;
     }
 }
