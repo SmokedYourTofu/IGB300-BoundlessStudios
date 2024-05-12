14cf9df57a4510373eef12bf8209f8ff67adba74
SmokedYourTofu <93462741+SmokedYourTofu@users.noreply.github.com>
2024-05-13 04:10:31 +1000

    working on dash penalty

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -49,7 +49,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction.normalized * moveSpeed * 10f, ForceMode.Force);
+        rb.AddForce(direction.normalized * moveSpeed * direction.magnitude * 10f, ForceMode.Force);
 
         if (direction != Vector3.zero) {
             Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
@@ -69,5 +69,9 @@ public class PlayerController : MonoBehaviour
 
         Vector3 dash = orientation.forward * dashForce;
         rb.AddForce(dash, ForceMode.Impulse);
+    }
+
+    public void Recovering(float newDrag) {
+
     }
 }
